4f7ed1f2f97a4c424768bc8c7ba0fd0b8c63a695
yunfengzhang.zyf <yunfengzhang.zyf@alibaba-inc.com>
2018-12-25 00:10:48 +0800

    iso

--- iso/myiso/kernel.cpp
+++ iso/myiso/kernel.cpp
@@ -1,5 +1,12 @@
-void kernelMain(void* multiboot_structure, unsigned int magicnumber)
+void print(char* str)
 {
-	printf("Hello World! yunfeng");
+	static unsigned short* VideoMemory =(unsigned short*)0xb8000;
+	for(int i=0;str[i] != '\0';++i)
+		VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+}
+
+extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber)
+{
+	print("Hello World! yunfeng");
 	while(1);
 }
